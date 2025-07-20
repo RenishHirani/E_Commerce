96b6cc25d784109254a3a6f7c0c2155e5f0de085
Renish Hirani <142030406+RenishHirani@users.noreply.github.com>
2025-07-20 23:42:20 +0530

    Update config.js

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://RenishHirani:3poQPuR3dHEXjfYL@ecommerce.nz7wi0x.mongodb.net/ecommerce")
+mongoose.connect("Your_Connection_String")
